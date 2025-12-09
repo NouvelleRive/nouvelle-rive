29c409e09db52aa97d75b92470642dbac1cd16fe
Salomé Kassabi <salomekassabi@MacBook-Pro-de-Salome.local>
2025-12-09 13:51:09 +0100

    fix: espaces insécables PDF facture

--- src/components/SalesList.tsx
+++ src/components/SalesList.tsx
@@ -412,8 +412,8 @@
       const code = getVendorCode(chineuse, ventesDuMois)
       const ref = `NR${String(m).padStart(2, '0')}${String(y).slice(-2)}-${code}`
 
-      const fmt = (n: number) => new Intl.NumberFormat('fr-FR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(n)
-      const fmtEUR = (n: number) => new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(n)
+      const fmt = (n: number) => new Intl.NumberFormat('fr-FR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(n).replace(/\u00A0/g, ' ')
+      const fmtEUR = (n: number) => new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(n).replace(/\u00A0/g, ' ')
 
       const docPDF = new jsPDF({ unit: 'pt', format: 'a4' })
       const pageW = docPDF.internal.pageSize.getWidth()
