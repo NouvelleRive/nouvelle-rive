d725dee6b5849c3d6b63f0e4241a5d912c5b7c7d
Salomé Kassabi <salomekassabi@MacBook-Air.local>
2026-03-06 13:26:34 +0100

    login: justify-between colonnes

--- src/app/(public)/client/login/page.tsx
+++ src/app/(public)/client/login/page.tsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen bg-white" style={{ fontFamily: fontHelvetica }}>
-      <div className="px-6 py-12 flex gap-12 items-start">
+      <div className="px-6 py-12 flex gap-12 items-start justify-between">
         <div className="flex-1">
         {/* Titre */}      
 
